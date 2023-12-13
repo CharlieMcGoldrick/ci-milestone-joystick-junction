a99ad5bf417fd735fe2821fcdad8596287a42a12
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-13 05:09:39 +0000

    Feat: add utils.py for twitch API access token

---
+++ message_board/utils.py
@@ -0,0 +1,15 @@
+import requests
+from django.conf import settings
+
+def get_twitch_access_token():
+    url = 'https://id.twitch.tv/oauth2/token'
+    payload = {
+        'client_id': settings.TWITCH_CLIENT_ID,
+        'client_secret': settings.TWITCH_CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, data=payload)
+    if response.status_code == 200:
+        return response.json()['access_token']
+    else:
+        return None
