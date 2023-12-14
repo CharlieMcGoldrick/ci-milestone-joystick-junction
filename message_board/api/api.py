bac81c961b846a8f0683a38512f00d45cdeec681
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-14 03:47:05 +0000

    Feat: add igdb api request

--- message_board/api/api.py
+++ message_board/api/api.py
@@ -14,3 +14,23 @@ def get_twitch_access_token():
         return response.json()['access_token']
     else:
         return None
+
+
+def make_igdb_api_request(endpoint, query_body):
+    access_token = get_twitch_access_token()
+    if access_token is None:
+        return None  # or handle the error as needed
+
+    url = f'https://api.igdb.com/v4/{endpoint}'  # Construct the full URL
+    headers = {
+        'Client-ID': settings.TWITCH_CLIENT_ID,
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.post(url, headers=headers, data=query_body)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        # Handle errors (such as logging them, raising exceptions, etc.)
+        return None
