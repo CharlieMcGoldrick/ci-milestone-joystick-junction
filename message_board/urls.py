c76087b55a28036b355ffbdd5a68cb972428d650
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-17 02:05:17 +0000

    Feat: add account_management.html template, url & view

--- message_board/urls.py
+++ message_board/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('account_management/', views.account_management, name='account_management')
 ]
--- message_board/views.py
+++ message_board/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     template_name = "index.html"
     return render(request, template_name)
+
+
+def account_management(request):
+    template_name = "account_management.html"
+    return render(request, template_name)
