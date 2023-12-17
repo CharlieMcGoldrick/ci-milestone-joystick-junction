61f5a8296230b43e453d97887ef5e55f426182db
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-17 23:34:46 +0000

    Test add green test for registration class

--- message_board/tests/test_forms.py
+++ message_board/tests/test_forms.py
@@ -4,13 +4,12 @@ from message_board.forms import RegisterForm
 class RegisterFormTest(TestCase):
 
     def test_register_form_valid(self):
-        # Red: Testing the form with valid data
+        # Green: Testing the form with valid data
         data = {
             "username": "testuser",
             "email": "testuser@example.com",
-            "password1": "password123",
-            "password2": "differentpassword123",
+            "password1": "password123?",
+            "password2": "password123?",
         }
         form = RegisterForm(data=data)
         self.assertTrue(form.is_valid())
-        
