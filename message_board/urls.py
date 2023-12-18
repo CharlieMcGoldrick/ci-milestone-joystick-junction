2e95994e1460f6ab0660ea57d9abb0e94ca0d5af
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 07:30:19 +0000

    Feat: update URL patterns for 'check_username_email'

--- message_board/urls.py
+++ message_board/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('account_management/', views.account_management, name='account_management')
+    path('account_management/', views.account_management, name='account_management'),
+    path('check_username_email', views.check_username_email, name='check_username_email'),
 ]
