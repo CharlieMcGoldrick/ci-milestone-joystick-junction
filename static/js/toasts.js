f9ff18387088b0ea71fd5ae5b46523aa1b74d3a0
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 07:23:59 +0000

    Feat: add improvements to client-side validation

--- static/js/toasts.js
+++ static/js/toasts.js
@@ -9,18 +9,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const toastBody = document.getElementById('toastBody');
 
     signupForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent form from submitting immediately
+
         // Check if the username is between 3 and 20 characters
         if (usernameInput.value.length < 3 || usernameInput.value.length > 20) {
             toastBody.textContent = 'Username must be between 3 and 20 characters.';
             errorToast.show();
-            event.preventDefault();
+            return;
         }
 
         // Check if the email includes an '@'
         if (!emailInput.value.includes('@')) {
             toastBody.textContent = 'Please include an "@" in the email address.';
             errorToast.show();
-            event.preventDefault();
+            return;
         }
 
         // Check if the password is at least 8 characters and includes a digit and a special character
@@ -28,14 +30,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if (password1.length < 8 || !/\d/.test(password1) || !/[!@#$%^&*]/.test(password1)) {
             toastBody.textContent = 'Password must be at least 8 characters and include at least one digit and one special character.';
             errorToast.show();
-            event.preventDefault();
+            return;
         }
 
         // Check if the passwords match
         if (password1 !== password2Input.value) {
             toastBody.textContent = 'Passwords do not match.';
             errorToast.show();
-            event.preventDefault();
+            return;
         }
+
+        // Check if the username or email is already taken
+        fetch('/check_username_email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify({
+                username: usernameInput.value,
+                email: emailInput.value
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.username_taken) {
+                    toastBody.textContent = 'Username is already taken.';
+                    errorToast.show();
+                } else if (data.email_taken) {
+                    toastBody.textContent = 'Email is already taken.';
+                    errorToast.show();
+                } else {
+                    // If username and email are not taken, submit the form
+                    signupForm.submit();
+                }
+            });
     });
 });
