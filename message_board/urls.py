6d948cc0b8c51e3ef0ecd23bd361850aab76f584
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 19:34:58 +0000

    Feat add URL pattern for admin promotion view

--- message_board/urls.py
+++ message_board/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('signup/', views.signup_view, name='signup_view'),
     path('check_username_email', views.check_username_email, name='check_username_email'),
+    path('promote_to_admin/', views.promote_to_admin, name='promote_to_admin'),
     path('logout/', views.logout_view, name='logout'),
 ]
