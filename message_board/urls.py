9f357e5d4cde68ae06f33af4efa816262bef90d0
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 15:54:10 +0000

    Feat: add login and signup URL patterns

--- message_board/urls.py
+++ message_board/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('account_management/', views.account_management, name='account_management'),
+    path('login/', views.login_view, name='login_view'),
+    path('signup/', views.signup_view, name='signup_view'),
     path('check_username_email', views.check_username_email, name='check_username_email'),
     path('logout/', views.logout_view, name='logout'),
 ]
