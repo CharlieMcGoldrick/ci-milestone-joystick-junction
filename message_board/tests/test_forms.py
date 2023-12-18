2f3aaed5dcdb2f086020f5ed8b0666014ef903c9
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 02:51:22 +0000

    Test: add red test for login class

--- message_board/tests/test_forms.py
+++ message_board/tests/test_forms.py
@@ -1,9 +1,26 @@
 from django.test import TestCase
-from message_board.forms import RegisterForm
+from django.contrib.auth import get_user_model
+from message_board.forms import LoginForm, SignupForm
 
-class RegisterFormTest(TestCase):
+class LoginFormTest(TestCase):
 
-    def test_register_form_valid(self):
+    def test_nonexistent_username(self):
+        # Red: Testing the form by trying a user with a different username
+        User = get_user_model()
+        User.objects.create_user(username='existing_user', password='testpassword')
+
+        form_data = {
+            'username': 'nonexistent_user',
+            'password': 'testpassword',
+        }
+
+        form = LoginForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class SignupFormTest(TestCase):
+
+    def test_signup_form_valid(self):
         # Green: Testing the form with valid data
         data = {
             "username": "testuser",
@@ -11,5 +28,5 @@ class RegisterFormTest(TestCase):
             "password1": "password123?",
             "password2": "password123?",
         }
-        form = RegisterForm(data=data)
+        form = SignupForm(data=data)
         self.assertTrue(form.is_valid())
