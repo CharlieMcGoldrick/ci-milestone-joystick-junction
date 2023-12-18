22aadb78344d2a6e1996c5a20b487c2a4240c405
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 16:24:43 +0000

    Refactor: change login url to use djangos version

--- message_board/urls.py
+++ message_board/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('account_management/', views.account_management, name='account_management'),
-    path('login/', views.login_view, name='login_view'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('signup/', views.signup_view, name='signup_view'),
     path('check_username_email', views.check_username_email, name='check_username_email'),
     path('logout/', views.logout_view, name='logout'),
