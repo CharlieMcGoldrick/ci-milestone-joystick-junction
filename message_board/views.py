8f32c4491dfe1f6f6b6343bd54ff9afda6df730c
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-18 07:28:57 +0000

    Feat: add user registration view endpoint

--- message_board/views.py
+++ message_board/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm, SignupForm 
+from .forms import LoginForm, SignupForm
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json 
 
 def home(request):
     template_name = "index.html"
@@ -11,21 +14,28 @@ def account_management(request):
     signup_form = SignupForm(request.POST or None)
 
     if request.method == 'POST':
-        print("POST request received")  # Debugging print statement
         if 'login' in request.POST:
-            print("Login form submitted")  # Debugging print statement
             if login_form.is_valid():
                 # Log the user in and redirect to the home page
                 return redirect('home')
-            else:
-                print(login_form.errors)
-        elif 'register' in request.POST:
-            print("Signup form submitted")  # Debugging print statement
+        elif 'signup' in request.POST:
             if signup_form.is_valid():
-                # Register the user and reload the page
+                # Signup the user and reload the page
                 signup_form.save()
                 return redirect('account_management')
-            else:
-                print(signup_form.errors)
 
     return render(request, 'account_management.html', {'login_form': login_form, 'signup_form': signup_form})
+
+
+def check_username_email(request):
+    data = json.loads(request.body)
+    username = data.get('username')
+    email = data.get('email')
+
+    data = {
+        'username_taken': User.objects.filter(username=username).exists(),
+        'email_taken': User.objects.filter(email=email).exists()
+    }
+
+    return JsonResponse(data)
+
