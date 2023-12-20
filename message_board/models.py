a18b9196733030bb9c605198b2f29950f75e4d53
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-20 01:30:02 +0000

    Feat: add basic MainThread model with fields

--- message_board/models.py
+++ message_board/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Model Status
 STATUS = ((0, "Draft"), (1, "Published"))
 
+class MainThread(models.Model):
+    name = models.CharField(max_length=200)
+    game_id = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
