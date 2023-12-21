e0cb0474221af876d96faa5e5a6482e6b2a937ee
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-21 10:07:11 +0000

    Fix: search_games_for_main_thread url path

--- message_board/urls.py
+++ message_board/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('signup/', views.signup_view, name='signup_view'),
     path('check_username_email', views.check_username_email, name='check_username_email'),
+    path('search_games_for_main_thread/', views.search_games_for_main_thread, name='search_games_for_main_thread'),
     path('create_game_main_thread/<int:game_id>/', views.create_game_main_thread, name='create_game_main_thread'),
     path('search_created_main_threads/', views.search_created_main_threads, name='search_created_main_threads'),
     path('promote_to_admin/', views.promote_to_admin, name='promote_to_admin'),
