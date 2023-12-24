3a8e5b8fa2a2fe73e3acba54341b1c23498297b9
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-24 10:35:48 +0000

    Fix: AJAX form submission for thread creation

--- static/js/main_thread_editor.js
+++ static/js/main_thread_editor.js
@@ -16,12 +16,33 @@ $(document).ready(function () {
                         listItem.addClass('first-result-item');
                     }
 
-                    var form = $('<form>').attr('method', 'post').attr('action', "{% url 'create_game_main_thread' 'id_placeholder' %}".replace('id_placeholder', game.id));
+                    var form = $('<form>').attr('method', 'post').attr('action', $('#searchFormFindGame').data('base-action-url') + game.id + '/');
+                    form.append($('<input>').attr('type', 'hidden').attr('name', 'csrfmiddlewaretoken').val(getCookie('csrftoken')));
                     form.append($('<input>').attr('type', 'hidden').attr('name', 'game_name').val(game.name));
                     form.append($('<button>').addClass('result-button btn btn-primary w-100').text('Create Main Thread ' + game.name));
 
                     listItem.append(form);
                     searchGamesForMainThread.append(listItem);
+                });
+
+                // Handle form submission via AJAX
+                searchGamesForMainThread.on('submit', 'form', function (e) {
+                    e.preventDefault();
+
+                    $.ajax({
+                        url: $(this).attr('action'),
+                        type: 'POST',
+                        data: $(this).serialize(),
+                        beforeSend: function (xhr) {
+                            xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+                        },
+                        success: function (data) {
+                            // Handle success
+                        },
+                        error: function (xhr, status, error) {
+                            // Handle error
+                        }
+                    });
                 });
             }
         });
