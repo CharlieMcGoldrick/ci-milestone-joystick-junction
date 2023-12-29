9d40ea18ef7067c71dc1ebc870f76c0ee540004c
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-29 22:19:01 +0000

    Style: add styling to homepage search results

--- static/js/homepage_main_thread_search.js
+++ static/js/homepage_main_thread_search.js
@@ -11,13 +11,24 @@ $(document).ready(function () {
 
                 if (data.no_results) {
                     // Display a message if no results were found
-                    resultsContainer.append("<p>No threads match your request</p>");
+                    var noResultsMessage = $("<p>")
+                        .addClass("mt-3")
+                        .text("No threads match your request");
+                    resultsContainer.append(noResultsMessage);
                 } else {
                     // Display the results
                     $.each(data.results, function (i, thread) {
-                        var listItem = $("<li>").addClass("result-item");
-                        listItem.append("<a href='" + thread.url + "'>" + thread.name + "</a>");
-                        resultsContainer.append(listItem);
+                        var link = $("<a>")
+                            .attr("href", thread.url)
+                            .addClass("result-link");
+                        var listItem = $("<li>")
+                            .addClass("btn result-button result-item w-100")
+                            .text(thread.name);
+                        link.append(listItem);
+                        if (i === 0) {
+                            listItem.addClass("mt-3");
+                        }
+                        resultsContainer.append(link);
                     });
                 }
             },
